b4c57761b072363ea2310628d354bcc3fc386b13
yangyang <450428985@qq.com>
2018-10-11 17:00:17 +0800

    test

---
+++ src/Test.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace YangYang;
+/**
+ * Created by PhpStorm.
+ * User: yangyang
+ * Date: 2018/10/11
+ * Time: 下午4:51
+ */
+
+class Test
+{
+    public function index()
+    {
+        echo 'hello word';
+    }
+}
